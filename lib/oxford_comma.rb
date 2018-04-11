a9f37530671082342185a94af7a9947fdde42698
Anuradha Khambete <anukhambete@gmail.com>
2018-04-11 17:46:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,9 @@ def oxford_comma(array)
   else
     arr_last=[]                    #initilise empty array
     arr_last[0] = array.last       #add last element of main array to new empty array
-    arr_last.unshift("and ")
+    arr_last.unshift("and ")       # add "and " as 0th index to new array 
     
-    array.insert(-2,arr_last.join(""))
+    array.insert(-2,arr_last.join("")) #join the elements of new array (and + fruit) and insert at 2nd last position in main array
     array.pop
     array.join(", ")
   end
