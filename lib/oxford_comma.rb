e3c6bcb81ed7398a3422d86dc475d031ccca269a
Anuradha Khambete <anukhambete@gmail.com>
2018-04-11 17:45:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
   elsif array.size == 2
   array.join(" and ")
   else
-    arr_last=[]                    #initilise empty a
-    arr_last[0] = array.last
+    arr_last=[]                    #initilise empty array
+    arr_last[0] = array.last       #add last element of main array to new empty array
     arr_last.unshift("and ")
     
     array.insert(-2,arr_last.join(""))
